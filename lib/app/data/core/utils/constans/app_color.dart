f803e98fbf66575c4c97029989049460dc9cd217
zihadsikder <zihadsikder1234@gmail.com>
2025-05-09 18:08:49 +0600

    setup color and text and common utils

---
+++ lib/app/data/core/utils/constans/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF06464);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFFFF675C);
+  static const Color textSecondary = Color(0xFF333333);
+  static const Color error = Color(0xFFFF4B4B);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'app/data/core/binding/controller_binder.dart';
+import 'app/data/core/utils/constans/app_sizer.dart';
 import 'app/routes/app_pages.dart';
 import 'firebase_options.dart';
 
@@ -11,10 +15,40 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+    MyApp(),
   );
+}
+class PlatformUtils {
+  static bool get isIOS =>
+      foundation.defaultTargetPlatform == TargetPlatform.iOS;
+  static bool get isAndroid =>
+      foundation.defaultTargetPlatform == TargetPlatform.android;
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          title: "Soulee Application",
+          debugShowCheckedModeBanner: false,
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          initialBinding: ControllerBinder(),
+          themeMode: ThemeMode.system,
+
+          defaultTransition: PlatformUtils.isIOS ? Transition.cupertino : Transition.fade,
+          locale: Get.deviceLocale,
+          builder: (context, child) => PlatformUtils.isIOS
+              ? CupertinoTheme(data: const CupertinoThemeData(), child: child!)
+              : child!,
+        );
+      },
+    );
+  }
+
 }
